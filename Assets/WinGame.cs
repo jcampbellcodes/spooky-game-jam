8b8b8a178ead4c7fbd8a0f05af1bf3865378fcef
Jack Campbell <jackcampbell@acm.org>
2017-10-21 15:17:15 -0500

    adding game loop

---
+++ Assets/WinGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour {
+
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject == player)
+		{
+			Debug.Log("won game");
+			SceneManager.LoadScene (0);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		// Destroy everything that leaves the trigger
+		//Destroy(other.gameObject);
+	}
+}
