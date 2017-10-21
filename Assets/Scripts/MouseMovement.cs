add0aa74207346a68660abe0c58644c0144b7830
JACQUELINE_LENO\admin <jacquelinepassehl@gmail.com>
2017-10-21 14:40:56 -0500

    made the mouse collider lock to the y axis

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -14,7 +14,12 @@ public class MouseMovement : MonoBehaviour
     void Update()
     {
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, distance_to_screen, Input.mousePosition.y));
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+        
+
     }
     void OnMouseDrag()
     {
