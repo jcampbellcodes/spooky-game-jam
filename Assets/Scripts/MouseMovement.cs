b6f1d5654d6b6188e36880c81b6da19540766c0c
JACQUELINE_LENO\admin <jacquelinepassehl@gmail.com>
2017-10-20 23:56:44 -0500

    added mouse moving game object functionality

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+    }
+    void OnMouseDrag()
+    {
+        //float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+    }
+}
